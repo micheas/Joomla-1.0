3324d32c231c3df84626b58a87f4dc40670fe200
stingrey <stingrey@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2006-03-08 10:26:56 +0000

    # Fixed artf3847 : A mere mistake in joomla_admin template
    git-svn-id: http://joomlacode.org/svn/joomla/development/releases/1.0@2706 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/templates/joomla_admin/cpanel.php
+++ administrator/templates/joomla_admin/cpanel.php
@@ -21,7 +21,7 @@ defined( '_VALID_MOS' ) or die( 'Restricted access' );
 	   <?php mosLoadAdminModules( 'icon', 0 ); ?>
 	</td>
 	<td width="45%" valign="top">
-		<div style="width=100%;">
+		<div style="width: 100%;">
 			<form action="index2.php" method="post" name="adminForm">
 			<?php mosLoadAdminModules( 'cpanel', 1 ); ?>
 			</form>

0318bba8a990ea0c284bdec49edd562cdb5acb87
stingrey <stingrey@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2006-05-29 03:25:07 +0000

    # Fixed [artf4812] : In footer.php (C) should be &copy;
    git-svn-id: http://joomlacode.org/svn/joomla/development/releases/1.0@3713 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- CHANGELOG.php
+++ CHANGELOG.php
@@ -57,6 +57,7 @@ Low Level Threat
 
 29-May-2006 Rey Gigataras
  # Fixed [topic,64994] : problem with related items
+ # Fixed [artf4812] : In footer.php (C) should be &copy;
 
 
 27-May-2006 Rey Gigataras

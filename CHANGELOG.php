5e3449a36378af25a06e23e4472ad950b74724e8
stingrey <stingrey@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2006-07-02 17:50:27 +0000

    # Fixed [artf5171] : mosGetParam Default value issue
    git-svn-id: http://joomlacode.org/svn/joomla/development/releases/1.0@4157 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- CHANGELOG.php
+++ CHANGELOG.php
@@ -44,6 +44,7 @@ Legend:
  # Fixed [artf5181] : 5 step for unrecoverable admin-page crash.
  # Fixed [artf5123] : Wrong name of function in joomla.cache.php
  # Fixed [artf5126] : includes/database.php uses deprecated function
+ # Fixed [artf5171] : mosGetParam Default value issue
  
 
 ---------------- 1.0.10 Stable Released -- [26-June-2006 00:00 UTC] ------------------
--- includes/joomla.php
+++ includes/joomla.php
@@ -2937,8 +2937,8 @@ function mosGetParam( &$arr, $name, $def=null, $mask=0 ) {
 				}
 				$return = $noHtmlFilter->process( $return );
 				
-				if (is_numeric($def)) {
-				// if default value is numeric set variable type to integer
+				if (empty($return) && isnumeric($def)) {
+				// if value is defined and default value is numeric set variable type to integer
 					$return = intval($return);
 				}				
 			}

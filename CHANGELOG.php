e7a3adc6bcd31547a6c252702941d6c9a0790b19
predator <predator@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2006-09-09 09:58:13 +0000

    "# Fixed [artf5753] : ampersand in action URL of showArchiveCategory form should be an entity "
    git-svn-id: http://joomlacode.org/svn/joomla/development/releases/1.0@4976 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- CHANGELOG.php
+++ CHANGELOG.php
@@ -37,6 +37,10 @@ code fixes.
 - -> Removed
 ! -> Note
 
+
+09-Sep-2006 Marko Schmuck
+ # Fixed [artf5753] : ampersand in action URL of showArchiveCategory form should be an entity
+
 06-Sep-2006 Marko Schmuck
  # Fixed [artf5367] : Better mysql statement in content.searchbot.php
  # Fixed [artf5141] : image attribute name="image" breaks xhtml compliance when output multiple times

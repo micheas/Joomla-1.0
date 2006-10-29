7b1ff296f34b22e3e56ba3a37b8a4b1037d049c6
facedancer <facedancer@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2006-10-29 10:54:02 +0000

    # Fixed templates/madeyourweb/images/indent1.png and indent2.png file size
    git-svn-id: http://joomlacode.org/svn/joomla/development/releases/1.0@5601 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- CHANGELOG.php
+++ CHANGELOG.php
@@ -39,6 +39,7 @@ code fixes.
 
 29-Oct-2006 Mateusz Krzeszowiec
  # Fixed [artf6132] : Admin Session not completely emptied on logout, also removed some code (doublecheck) in administrator/logout.php
+ # Fixed templates/madeyourweb/images/indent1.png and indent2.png file size
 
 26-Oct-2006 Emir Sakic
  ^ Removed version check - [artf6486] : Remove "Your Joomla! Installation is ... days old" messages

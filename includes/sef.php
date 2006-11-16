444e81dc4a84819234b183d2b4bc0bcc758c99f1
predator <predator@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2006-11-16 14:01:20 +0000

    "removed added url_schemes and added them in joomla.php 106"
    git-svn-id: http://joomlacode.org/svn/joomla/development/releases/1.0@5797 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- includes/sef.php
+++ includes/sef.php
@@ -530,11 +530,7 @@ function sefRelToAbs( $string ) {
 				$url_schemes 	= explode( ', ', _URL_SCHEMES );
 				$url_schemes[] 	= 'http:';
 				$url_schemes[] 	= 'https:';
-				$url_schemes[] 	= 'ftp:';
-				$url_schemes[] 	= 'file:';
-				$url_schemes[] 	= 'mailto:';
-				$url_schemes[] 	= 'mms:';
-
+				
 				foreach ( $url_schemes as $url ) {
 					if ( strpos( $string, $url ) === 0 ) {
 						$check = 0;

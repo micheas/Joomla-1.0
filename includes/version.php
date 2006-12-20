d2bce0030d1fdcef76a8624f019b4d6ba4eaa23d
robs <robs@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2006-12-20 18:55:12 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/releases/1.0@6073 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- includes/version.php
+++ includes/version.php
@@ -24,7 +24,7 @@ class joomlaVersion {
 	/** @var int Main Release Level */
 	var $RELEASE 	= '1.0';
 	/** @var string Development Status */
-	var $DEV_STATUS = 'RC';
+	var $DEV_STATUS = 'RC 2';
 	/** @var int Sub Release Level */
 	var $DEV_LEVEL 	= '12';
 	/** @var int build Number */
@@ -32,9 +32,9 @@ class joomlaVersion {
 	/** @var string Codename */
 	var $CODENAME 	= 'Sunfire';
 	/** @var string Date */
-	var $RELDATE 	= '13 December 2006';
+	var $RELDATE 	= '20 December 2006';
 	/** @var string Time */
-	var $RELTIME 	= '21:00';
+	var $RELTIME 	= '20:00';
 	/** @var string Timezone */
 	var $RELTZ 		= 'UTC';
 	/** @var string Copyright Text */
